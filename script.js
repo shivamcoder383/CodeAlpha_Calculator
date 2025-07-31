d4680090de6fd4015f2913d574ed1ef67cfa799a
shivamcoder383 <shiva01pa@gmail.com>
2025-07-31 20:29:46 +0530

    Add files via upload
    This is my calculator project@codeAlpha

---
+++ script.js
@@ -0,0 +1,36 @@
+
+let display = document.getElementById('display');
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+// Keyboard support
+document.addEventListener('keydown', function(event) {
+    const key = event.key;
+    if ((/\d|[+\-*/.%]/).test(key)) {
+        appendValue(key);
+    } else if (key === 'Enter') {
+        calculate();
+    } else if (key === 'Backspace') {
+        deleteLast();
+    } else if (key.toLowerCase() === 'c') {
+        clearDisplay();
+    }
+});
